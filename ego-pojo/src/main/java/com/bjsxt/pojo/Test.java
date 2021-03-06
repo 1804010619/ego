66bb6c8fa5a8afe53a6bb47dade93f6f719d7633
wanglimin <212886089@qq.com>
2021-03-06 22:39:21 +0800

    hello git

---
+++ ego-pojo/src/main/java/com/bjsxt/pojo/Test.java
@@ -0,0 +1,11 @@
+package com.bjsxt.pojo;
+
+/**
+ * @author wanglimin
+ * @date 2021-03-06 10:38 下午
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello Git");
+	}
+}
