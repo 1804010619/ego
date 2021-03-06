c121d046b68734e1ade69eac592d545975cd510c
wanglimin <212886089@qq.com>
2021-03-06 22:55:23 +0800

    输出hello

--- ego-pojo/src/main/java/com/bjsxt/pojo/Test.java
+++ ego-pojo/src/main/java/com/bjsxt/pojo/Test.java
@@ -8,6 +8,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
 		System.out.println("hello");
-		System.out.println("1");
+		System.out.println("2");
 	}
 }
